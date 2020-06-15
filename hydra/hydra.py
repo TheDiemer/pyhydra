5afe0f57c8e71270225a758ba30b6d13069db23b
Eric Jones <TheDiemer@users.noreply.github.com>
2020-06-15 14:20:37 -0400

    Search_case to Kwargs (#13)
    * implementing inital kwarg work. No safety protection, error checking or anything yet.
    
    * handling incoming data better, checking for lists or not
    
    * added list checking
    
    * exception handling for get api
    
    * getting an empty response instead of Just an error now, also removed some parantheses that were causing issues in search. Currently seems to be able to do one query at a time. need to work with the requests manually to see what needs to change for multiple-part query
    
    * removing the finally return as it hid the exceptions which were intentional. Also figured out the syntax for adding Multiple queries (such as all cases from a specific SBR created during a specific date range
    
    * implementing inital kwarg work. No safety protection, error checking or anything yet.
    
    * handling incoming data better, checking for lists or not
    
    * added list checking
    
    * exception handling for get api
    
    * getting an empty response instead of Just an error now, also removed some parantheses that were causing issues in search. Currently seems to be able to do one query at a time. need to work with the requests manually to see what needs to change for multiple-part query
    
    * removing the finally return as it hid the exceptions which were intentional. Also figured out the syntax for adding Multiple queries (such as all cases from a specific SBR created during a specific date range
    
    * fully caught up with the deprecation branch and retested this. works as expected with the kwargs
    
    * renaming to kwargs to match convention
    
    Co-authored-by: Eric Jones <erjones@tael.usersys.redhat.com>

--- hydra/hydra.py
+++ hydra/hydra.py
@@ -53,20 +53,38 @@ class hydra_api:
         # r = s.send(prepared)
 
         # NOTE: Comment this out; if you use the block above.
-        r = requests.get(
-            "{}/{}".format(self.base_api_uri, endpoint),
-            params=parameters,
-            headers=headers,
-            auth=authentication,
-            verify=self.ca_certificate_path,
-        )
-        if r.status_code == 204:
-            return []
-        if r.status_code != 200:
+        try:
+            r = requests.get(
+                "{}/{}".format(self.base_api_uri, endpoint),
+                params=parameters,
+                headers=headers,
+                auth=authentication,
+                verify=self.ca_certificate_path,
+            )
+            if r.status_code == 204:
+                return []
+            if r.status_code != 200:
+                raise Exception(
+                    """looking up infomation from: {}\n
+                        Error Code: {}""".format(
+                        endpoint, r.status_code
+                    )
+                )
+            try:
+                return r.json()
+            except:
+                response = []
+                raise Exception(
+                    """looking up information from: {}\n
+                    Error returning json""".format(
+                        endpoint
+                    )
+                )
+        except Exception:
             raise Exception(
-                """looking up infomation from: {}\n
-                    Error Code: {}""".format(
-                    endpoint, r.status_code
+                """looking up information from: {}\n
+                Error during request call""".format(
+                    endpoint
                 )
             )
 
@@ -473,55 +491,34 @@ class hydra_api:
 
         return self.__get_api("cases/", parameters=query_params)
 
-    def search_cases(
-        self,
-        fields=[],
-        accounts=[],
-        cases=[],
-        sbrGroups=[],
-        ownerSsousername=[],
-        tags=[],
-        cluster_ids=[],
-    ):
+    def search_cases(self, **kwargs):
 
         query_params = {}
-        ## Pulls back a lot if your not filtering! A field filter is recommended.
-        if fields:
-            query_params.update({"fl": ",".join(fields)})
-
-        ## Mutually Exclusive Search!!!
-        ## You can only search based on 1 paramiter! accounts, cases, etc.
-
-        if accounts:
-            query_params.update(
-                {"q": "case_accountNumber:({})".format(" OR ".join(accounts))}
-            )
-
-        if cases:
-            query_params.update({"q": "case_number:({})".format(" OR ".join(cases))})
-
-        if ownerSsousername:
-            query_params.update(
-                {
-                    "q": "case_owner_sso_username:({})".format(
-                        " OR ".join(ownerSsousername)
-                    )
-                }
-            )
-
-        if sbrGroups:
-            query_params.update({"q": "case_sbr:({})".format(" OR ".join(sbrGroups))})
-
-        if tags:
-            query_params.update({"q": "case_tags:({})".format(" OR ".join(tags))})
-
-        if cluster_ids:
-            query_params.update(
-                {"q": "case_openshift_cluster_id:({})".format(" OR ".join(cluster_ids))}
-            )
+        # Looping back over the incoming kwargs
+        for k, v in params.items():
+            ## Fields add a much needed filter as the query will return a LOT of information without some!
+            if k.lower() == "fl":
+                query_params.update({"fl": ",".join(v)})
+            # Everything else is a Query so it goes under q
+            else:
+                # Modifying the way the value is handed to the dictionary if handed a list or not
+                if isinstance(v, list):
+                    if "q" in query_params.keys():
+                        query_params.update(
+                            {"fq": "{0}:({1})".format(k, " OR ".join(v))}
+                        )
+                    else:
+                        query_params.update(
+                            {"q": "{0}:({1})".format(k, " OR ".join(v))}
+                        )
+                else:
+                    if "q" in query_params.keys():
+                        query_params.update({"fq": "{0}:{1}".format(k, v)})
+                    else:
+                        query_params.update({"q": "{0}:{1}".format(k, v)})
 
         return self.__get_api(
             "search/cases/",
             parameters=query_params,
-            headers={"Content-Type": "application/json",},
+            headers={"Content-Type": "application/json"},
         )
