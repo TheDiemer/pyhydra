57123e4bbea579f60651d241fb81ff14088f3ed0
Eric Rich <erich@redhat.com>
2020-06-18 08:22:59 -0400

    We need to fix a url encoding issue for string with spaces in them.  (#16)
    * fixing an issue where paging paramiters are added to queries.
    
    * fixing url encoding issues with spaces
    
    * fixing url encoding issues with spaces

--- hydra/hydra.py
+++ hydra/hydra.py
@@ -500,12 +500,12 @@ class hydra_api:
                 query_params.update({"fl": ",".join(v)})
             elif k.lower() == "start":
                 query_params.update({"start": v})
-                pass
             elif k.lower() == "rows":
                 query_params.update({"rows": v})
             else:
                 # Modifying the way the value is handed to the dictionary if handed a list or not
                 if isinstance(v, list):
+                    v = [f'"{x}"' for x in v]
                     if "q" in query_params.keys():
                         query_params.update(
                             {"fq": "{0}:({1})".format(k, " OR ".join(v))}
