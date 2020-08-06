bf03305491d39ee756759ebbb6fe861fdeeb88f8
Eric Jones <erjones@tael.usersys.redhat.com>
2020-08-06 12:47:20 -0400

    updating distributions and version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyhydra",
-    version="0.1.0",
+    version="0.2.0",
     author="Eric Rich",
     author_email="erich@redhat.com",
     description="Python API for interacting with Hydra",
